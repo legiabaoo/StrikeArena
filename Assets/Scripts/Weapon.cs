90fb21976ece065526f6960bef58cdd8d678c309
gnuodiahtThaiDuong <phungthaiduong2020@gmail.com>
2024-10-18 13:44:16 +0700

    sửa băn súng

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,127 +1,145 @@
-    using System;
-    using System.Collections;
-    using System.Collections.Generic;
-    using UnityEngine;
-    using Photon.Pun;
-    using TMPro;
-    using Photon.Pun.UtilityScripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+using Photon.Pun.UtilityScripts;
 
-    public class Weapon : MonoBehaviour
+public class Weapon : MonoBehaviour
+{
+    public int damege;
+    public float fireRate;
+    public Camera camera;
+    private float nextFire;
+
+    [Header("Hieu Ung")]
+    public GameObject hitVFX;
+    [Header("So Dan")]
+    public int mag = 5;
+    public int ammo = 30;
+    public int magAmmo = 30;
+    [Header("UI")]
+    public TextMeshProUGUI magText;
+    public TextMeshProUGUI ammoText;
+
+    [Header("Do Giat")]
+    [Range(0f, 2f)]
+    public float recoverPercent = 0.7f;
+    [Space]
+    public float recoilUp = 1f;
+    public float recoilBack = 0f;
+
+    private Vector3 origianlPosition;
+    private Vector3 recoilVeclocity = Vector3.zero;
+
+    private bool recoiling;
+    public bool recovering;
+    private float recoilLength;
+    private float recoverLength;
+    public float doxa = 0f;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        public int damege;
-        public float fireRate;
-        public Camera camera;
-        private float nextFire;
+        magText.text = mag.ToString();
+        ammoText.text = ammo + "/" + magAmmo;
 
-        [Header("Hieu Ung")]
-        public GameObject hitVFX;
-        [Header("So Dan")]
-        public int mag = 5;
-        public int ammo = 30;
-        public int magAmmo = 30;
-        [Header("UI")]
-        public TextMeshProUGUI magText;
-        public TextMeshProUGUI ammoText;
-        /*  [Header("HieuUng Thay Dan")]
-          public Animation animation;
-          public AnimationClip reload;*/
+        origianlPosition = transform.localPosition;
 
-        [Header("Do Giat")]
-    /*    [Range(0f, 1f)]
-        public float recoilPercent = 0.3f;*/
-        [Range(0f, 2f)]
-        public float recoverPercent = 0.7f;
-        [Space]
-        public float recoilUp = 1f;
-        public float recoilBack = 0f;
+        recoilLength = 0;
+        recoverLength = 1 / fireRate * recoverPercent;
+    }
 
-        private Vector3 origianlPosition;
-        private Vector3 recoilVeclocity = Vector3.zero;
-
-        private bool recoiling;
-        public bool recovering;
-        private float recoilLength;
-        private float recoverLength;
-        public float doxa = 0f;
-        // Start is called before the first frame update
-        void Start()
+    // Update is called once per frame
+    void Update()
+    {
+        if (nextFire > 0)
         {
+            nextFire -= Time.deltaTime;
+        }
+
+        if (Input.GetButton("Fire1") && nextFire <= 0 && ammo > 0)
+        {
+            Debug.Log("ban ne...");
+            nextFire = 1 / fireRate;
+            ammo--;
             magText.text = mag.ToString();
             ammoText.text = ammo + "/" + magAmmo;
-
-            origianlPosition = transform.localPosition;
-
-
-            recoilLength = 0;
-            recoverLength = 1 / fireRate * recoverPercent;
+            Fire();
         }
 
-        // Update is called once per frame
-        void Update()
+        if (Input.GetKeyDown(KeyCode.R) && mag > 0)
         {
-            if (nextFire > 0)
-            {
-                nextFire -=Time.deltaTime;
-
-            }
-            if(Input.GetButton("Fire1") && nextFire <= 0 && ammo >0 /*&& animation.isPlaying == false*/)
-            {
-                Debug.Log("ban ne...");
-                nextFire = 1 / fireRate;
-                ammo--;
-                magText.text = mag.ToString();
-                ammoText.text = ammo + "/" + magAmmo;
-                Fire();
-
-            }
-            if (Input.GetKeyDown(KeyCode.R)&& mag > 0)
-            {
-                Reload();
-            }
-            if (recoiling)
-            {
-                Recoil();
-            }
-            if (recovering)
-            {
-                Recovering();
-            }
+            Reload();
         }
-        void Reload()
+
+        if (recoiling)
         {
-           /* animation.Play(reload.name);*/
-            if(mag > 0)
-            {
-                mag--;
-
-                ammo = magAmmo;
-            }
-            magText.text = mag.ToString();
-            ammoText.text = ammo + "/" + magAmmo;
+            Recoil();
         }
+
+        if (recovering)
+        {
+            Recovering();
+        }
+    }
+
+    void Reload()
+    {
+        if (mag > 0)
+        {
+            mag--;
+            ammo = magAmmo;
+        }
+        magText.text = mag.ToString();
+        ammoText.text = ammo + "/" + magAmmo;
+    }
+
     private void Fire()
     {
         recoiling = true;
         recovering = false;
+
+        // L?y PhotonView c?a ng??i b?n (shooter)
+        PhotonView shooterPhotonView = GetComponentInParent<PhotonView>();
+        if (shooterPhotonView == null || !shooterPhotonView.Owner.CustomProperties.ContainsKey("team"))
+        {
+            Debug.LogError("PhotonView ho?c CustomProperties không h?p l?. H?y b?n.");
+            return;
+        }
+
+        // L?y team c?a ng??i b?n
+        int shooterTeam = (int)shooterPhotonView.Owner.CustomProperties["team"];
+
+        // T?o ray t? camera
         Ray ray = new Ray(camera.transform.position, camera.transform.forward);
         RaycastHit hit;
 
-        // L?y team c?a ng??i b?n t? Player
-        int shooterTeam = (int)GetComponentInParent<PhotonView>().Owner.CustomProperties["team"];
-
+        // Ki?m tra va ch?m v?i raycast
         if (Physics.Raycast(ray.origin, ray.direction, out hit, doxa))
         {
             PhotonNetwork.Instantiate(hitVFX.name, hit.point, Quaternion.identity);
+
+            // L?y component "health" c?a ??i t??ng b? b?n
             health targetHealth = hit.transform.gameObject.GetComponent<health>();
 
             if (targetHealth != null)
             {
-                // L?y team c?a ng??i b? b?n
-                int targetTeam = (int)hit.transform.gameObject.GetComponent<PhotonView>().Owner.CustomProperties["team"];
+                // L?y PhotonView c?a ??i t??ng b? b?n
+                PhotonView targetPhotonView = hit.transform.gameObject.GetComponent<PhotonView>();
+                if (targetPhotonView == null || !targetPhotonView.Owner.CustomProperties.ContainsKey("team"))
+                {
+                    Debug.LogError("PhotonView ho?c CustomProperties c?a m?c tiêu không h?p l?.");
+                    return;
+                }
 
-                // Ki?m tra team tr??c khi gây sát th??ng
+                // L?y team c?a ??i t??ng b? b?n
+                int targetTeam = (int)targetPhotonView.Owner.CustomProperties["team"];
+
+                // Ki?m tra n?u khác team m?i cho phép gây sát th??ng
                 if (shooterTeam != targetTeam)
                 {
+                    // Gây sát th??ng cho m?c tiêu
                     PhotonNetwork.LocalPlayer.AddScore(damege);
                     if (damege >= targetHealth.healths)
                     {
@@ -129,36 +147,43 @@
                         RoomManager.instance.SetHashes();
                         PhotonNetwork.LocalPlayer.AddScore(100);
                     }
-                    hit.transform.gameObject.GetComponent<PhotonView>().RPC("TakeDamege", RpcTarget.All, damege);
+
+                    // G?i RPC ?? c?p nh?t sát th??ng lên t?t c? các client
+                    targetPhotonView.RPC("TakeDamege", RpcTarget.All, damege);
                 }
                 else
                 {
                     Debug.Log("Không th? b?n ??ng ??i.");
                 }
             }
+            else
+            {
+                Debug.LogError("??i t??ng không có component health.");
+            }
         }
     }
 
     void Recoil()
-        {
-            Vector3 finalPositon = new Vector3(origianlPosition.x, origianlPosition.y + recoilUp, origianlPosition.z - recoilBack);
-            transform.localPosition = Vector3.SmoothDamp(transform.localPosition, finalPositon, ref recoilVeclocity, recoilLength);
+    {
+        Vector3 finalPositon = new Vector3(origianlPosition.x, origianlPosition.y + recoilUp, origianlPosition.z - recoilBack);
+        transform.localPosition = Vector3.SmoothDamp(transform.localPosition, finalPositon, ref recoilVeclocity, recoilLength);
 
-            if(transform.localPosition == finalPositon)
-            {
-                recoiling = false;
-                recovering = true;
-            }
-        }
-        void Recovering()
+        if (transform.localPosition == finalPositon)
         {
-            Vector3 finalPositon = origianlPosition;
-            transform.localPosition = Vector3.SmoothDamp(transform.localPosition, finalPositon, ref recoilVeclocity, recoverLength);
-
-            if (transform.localPosition == finalPositon)
-            {
-                recoiling = false;
-                recovering = false;
-            }
+            recoiling = false;
+            recovering = true;
         }
     }
+
+    void Recovering()
+    {
+        Vector3 finalPositon = origianlPosition;
+        transform.localPosition = Vector3.SmoothDamp(transform.localPosition, finalPositon, ref recoilVeclocity, recoverLength);
+
+        if (transform.localPosition == finalPositon)
+        {
+            recoiling = false;
+            recovering = false;
+        }
+    }
+}
