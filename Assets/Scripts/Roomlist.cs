20a009283f6335a41c3585f8f5d82421b1ab3988
gnuodiahtThaiDuong <phungthaiduong2020@gmail.com>
2024-10-30 22:32:25 +0700

    a

--- Assets/Scripts/Roomlist.cs
+++ Assets/Scripts/Roomlist.cs
@@ -11,6 +11,7 @@ public class Roomlist : MonoBehaviourPunCallbacks
 
     public GameObject roomManagerGameobject;
     public RoomManager roomManager; 
+
     [Header("UI")]
     public Transform roomListParent;
     public GameObject roomListItemPrefab;
@@ -61,61 +62,61 @@ public class Roomlist : MonoBehaviourPunCallbacks
 
         PhotonNetwork.JoinLobby();
     }
+    /*  public override void OnRoomListUpdate(List<RoomInfo> roomList)
+      {
+          // T?o m?t danh sách m?i ?? l?u tr? danh sách phòng ?ã c?p nh?t
+          List<RoomInfo> updatedRoomList = new List<RoomInfo>(roomList);
+
+          foreach (var room in roomList)
+          {
+              Debug.Log("Room Name: " + room.Name);
+              // Tìm ki?m phòng trong danh sách cachedRoomList
+              int index = cachedRoomList.FindIndex(r => r.Name == room.Name);
+              if (index != -1)
+              {
+                  // N?u phòng ?ã t?n t?i trong danh sách cachedRoomList
+                  if (room.RemovedFromList)
+                  {
+                      // N?u phòng ?ã b? xóa kh?i danh sách, lo?i b? nó kh?i danh sách c?p nh?t
+                      updatedRoomList.RemoveAt(index);
+                  }
+                  else
+                  {
+                      // N?u không, c?p nh?t thông tin c?a phòng trong danh sách c?p nh?t
+                      updatedRoomList[index] = room;
+                  }
+              }
+              else
+              {
+                  // N?u phòng không t?n t?i trong danh sách cachedRoomList và không b? xóa kh?i danh sách,
+                  // thêm nó vào danh sách c?p nh?t
+                  if (!room.RemovedFromList)
+                  {
+                      updatedRoomList.Add(room);
+                  }
+              }
+          }
+
+          // C?p nh?t cachedRoomList v?i danh sách ?ã c?p nh?t
+          cachedRoomList = updatedRoomList;
+
+          // C?p nh?t giao di?n ng??i dùng
+          UpdateUI();
+      }*/
+
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
-        // T?o m?t danh sách m?i ?? l?u tr? danh sách phòng ?ã c?p nh?t
-        List<RoomInfo> updatedRoomList = new List<RoomInfo>(roomList);
 
-        foreach (var room in roomList)
-        {
-            Debug.Log("Room Name: " + room.Name);
-            // Tìm ki?m phòng trong danh sách cachedRoomList
-            int index = cachedRoomList.FindIndex(r => r.Name == room.Name);
-            if (index != -1)
-            {
-                // N?u phòng ?ã t?n t?i trong danh sách cachedRoomList
-                if (room.RemovedFromList)
-                {
-                    // N?u phòng ?ã b? xóa kh?i danh sách, lo?i b? nó kh?i danh sách c?p nh?t
-                    updatedRoomList.RemoveAt(index);
-                }
-                else
-                {
-                    // N?u không, c?p nh?t thông tin c?a phòng trong danh sách c?p nh?t
-                    updatedRoomList[index] = room;
-                }
-            }
-            else
-            {
-                // N?u phòng không t?n t?i trong danh sách cachedRoomList và không b? xóa kh?i danh sách,
-                // thêm nó vào danh sách c?p nh?t
-                if (!room.RemovedFromList)
-                {
-                    updatedRoomList.Add(room);
-                }
-            }
-        }
-
-        // C?p nh?t cachedRoomList v?i danh sách ?ã c?p nh?t
-        cachedRoomList = updatedRoomList;
-
-        // C?p nh?t giao di?n ng??i dùng
-        UpdateUI();
-    }
-
-   /* public override void OnRoomListUpdate(List<RoomInfo> roomList)
-    {
-       
         if (cachedRoomList.Count <= 0)
         {
             cachedRoomList = roomList;
         }
         else
         {
-            foreach(var room in roomList)
+            foreach (var room in roomList)
             {
                 Debug.Log("Room Name: " + room.Name);
-                for (int i = 0; i< cachedRoomList.Count; i++)
+                for (int i = 0; i < cachedRoomList.Count; i++)
                 {
                     if (cachedRoomList[i].Name == room.Name)
                     {
@@ -127,7 +128,7 @@ public class Roomlist : MonoBehaviourPunCallbacks
                         }
                         else
                         {
-                            newList[i]= room;
+                            newList[i] = room;
                         }
                         cachedRoomList = newList;
 
@@ -138,7 +139,7 @@ public class Roomlist : MonoBehaviourPunCallbacks
         }
         UpdateUI();
     }
-*/
+
     private void UpdateUI()
     {
         Debug.Log("Number of rooms in cached list: " + cachedRoomList.Count);
