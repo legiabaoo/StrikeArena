fa2ae32bc9a5e59d89390985e43989c85f39488f
phvc8104 <nhp08102004@gmail.com>
2024-12-13 08:46:36 +0700

    sua conflict

---
+++ Assets/Resources/RuongGiap/UseChest.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class UseChest : MonoBehaviourPunCallbacks
+{
+    private GameObject OB;
+    public GameObject handUI;
+    public int soLuongGiap = 30; // Số lượng giáp cộng thêm
+    public int cost = 50; // Chi phí mở rương
+    private bool isUsed = false;
+    private bool inReach;
+
+    [Header("UI")]
+    public Text moneyText;
+
+    private int playerMoney = 100;
+
+    private void Start()
+    {
+        OB = this.gameObject;
+        handUI.SetActive(false);
+
+        // Cập nhật UI tiền ban đầu
+        UpdateMoneyUI(playerMoney);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach" && !isUsed)
+        {
+            inReach = true;
+            handUI.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            handUI.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (inReach && !isUsed && Input.GetKey(KeyCode.E))
+        {
+            if (playerMoney >= cost)
+            {
+                OpenChest();
+            }
+            else
+            {
+                Debug.Log("Không đủ tiền để mở rương!");
+            }
+        }
+    }
+
+    // Cập nhật UI tiền
+    void UpdateMoneyUI(int newMoney)
+    {
+        if (moneyText != null)
+        {
+            moneyText.text = "Money: " + newMoney.ToString(); // Cập nhật UI Text
+        }
+    }
+
+    // Phương thức mở rương và chỉ cập nhật giáp cho người nhặt rương
+    void OpenChest()
+    {
+        health playerHealth = FindObjectOfType<health>();
+        playerHealth.AddArmor(soLuongGiap);
+        // Thực hiện kiểm tra chỉ người chơi nhặt rương thực sự nhận giáp
+        if (PhotonNetwork.LocalPlayer != null)
+        {
+            // Lấy player từ PhotonNetwork
+
+            if (playerHealth != null)
+            {
+                // Cộng giáp cho người chơi local
+
+                // Trừ tiền cho người chơi
+                playerMoney -= cost;
+                // Cập nhật lại UI tiền sau khi trừ
+                UpdateMoneyUI(playerMoney);
+                Debug.Log("Mở rương thành công! Số tiền còn lại: " + playerMoney);
+
+                // Đánh dấu rương đã được mở
+                isUsed = true;
+                handUI.SetActive(false);
+                OB.GetComponent<Animator>().SetBool("open", true); // Chạy animation mở rương
+            }
+        }
+    }
+}
