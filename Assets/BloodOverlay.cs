0e1140606ef98d8d2543882060fdb1ad8259ecd0
gnuodiahtThaiDuong <phungthaiduong2020@gmail.com>
2024-12-17 18:40:25 +0700

    a

--- Assets/BloodOverlay.cs
+++ Assets/BloodOverlay.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BloodOverlay : MonoBehaviour
 {
     public GameObject bloodOverlay; 
     public float displayTime = 0.5f;  
-    private bool isActive = false; 
+    private bool isActive = false;
+    public GameObject handUI;
 
- 
     void Start()
     {
         if (bloodOverlay != null)
         {
             bloodOverlay.SetActive(false); 
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Side")
+        {
+           
+            handUI.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Side")
+        {
+           
+            handUI.SetActive(false);
         }
     }
 
